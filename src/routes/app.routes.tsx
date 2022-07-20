a8f5effbf01d563a8b16d2f0fc2bb70f7c53ffd8
Nadilson <nadilson@protonmail.com>
2022-07-20 02:52:01 -0300

    App navigation

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -9,7 +9,7 @@ export function AppRoutes() {
   return (
     <Navigator screenOptions={{ headerShown: false }}>
       <Screen name="home" component={Home} />
-      <Screen name="register" component={Register} />
+      <Screen name="new" component={Register} />
       <Screen name="details" component={Details} />
     </Navigator>
   );
